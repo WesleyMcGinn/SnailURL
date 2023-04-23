256ae73027f275aa591bc20f0ab0b6f7b82af9a3
Wesley McGinn <wesleymcginn1@gmail.com>
2023-04-23 10:31:29 -0400

    Add security system

--- setup.js
+++ setup.js
@@ -1,5 +1,6 @@
 function setup() {
     user.rememberThis();
+    securityCheck.action();
     setBackgroundColor();
     if (!showMessage()) {
         if (urlParameters.get("goNow") == "disabled") {
@@ -93,7 +94,13 @@ function tryPass() {
             setRedirectTimeout();
         } else {
             localStorage.wrongs++;
+            securityCheck.action();
             location.assign("file:///Users/24wmcginn/Documents/GitHub/SnailURL/home.html?m=%3Ch1%3EYOU%20ENTERED%20THE%20WRONG%20PASSWORD!%3C/h1%3E%3Cp%3EIf%20SnailURL%20detects%20too%20much%20suspicious%20activity%20on%20this%20device,%20you%20may%20eventually%20be%20%3Cb%3EBLOCKED%3C/b%3E%20from%20several%20features%20on%20the%20site.%20%3Ci%3EPlease%20be%20cautious.%3C/i%3E%3C/p%3E%3Cbr%3E%3Cbr%3E%3Cdiv%20id=%27a%27%20class=%27box%27%3E...%3C/div%3E&C=black&c=red&script=document.getElementById(%27a%27).innerHTML%20=%20localStorage.wrongs");
         }
     }
+}
+
+function leave() {
+    window.close();
+    location.href = "about:blank";
 }
--- userSystem.js
+++ userSystem.js
@@ -29,6 +29,7 @@ var user = {
             ThisSnailsTimes = [];
             localStorage.rights = 0;
             localStorage.wronts = 0;
+            localStorage.randomID = Math.random();
         } else {
             ThisSnailsData = eval(localStorage.thisSnailsData);
             ThisSnailsTimes = eval(localStorage.thisSnailsTimes);
@@ -38,4 +39,72 @@ var user = {
         localStorage.thisSnailsData = JSON.stringify(ThisSnailsData);
         localStorage.thisSnailsTimes = JSON.stringify(ThisSnailsTimes);
     }
+}
+
+var securityCheck = {
+    errorPercentage : 100*(localStorage.wrongs / (localStorage.rights + localStorage.wrongs)),
+    errorRatio : (localStorage.wrongs / localStorage.rights),
+    dangerLevel : function() {
+        if (localStorage.wrongs == 0) {
+            return 0; // No danger
+        } else if (localStorage.wrongs < 4) {
+            return 1; // Minimal danger
+        } else if (localStorage.wrongs < 10) {
+            return 2; // Potential danger
+        } else if (localStorage.wrongs < 20) {
+            return 3; // Real danger
+        } else if (localStorage.wrongs < 50) {
+            return 4; // Significant danger
+        } else {
+            return 5; // Extreme danger
+        }
+    },
+    concern : (this.errorRatio > 1 && this.dangerLevel > 1),
+    NOTIFY : function() {
+        Email.send({
+            Host: "smtp.elasticemail.com",
+            Username: "wesleymcginn1@gmail.com",
+            Password: "F0C825FA1ABA268D00626F48DFCD4BBD1254",
+            To: 'wesleymcginn1@gmail.com',
+            From: "wesleymcginn1@gmail.com",
+            Subject: "SnailURL: Suspicious Activity Report for User " + localStorage.randomID.toString(),
+            Body: "User " + localStorage.randomID.toString() + " was caught in suspicious activity on SnailURL.<br>Here are the statistics:<br><br><table style='border-style: solid; border-width: 2px'><tr><td>Wrong Passwords:</td><td>Right Passwords:</td><td>Frequency:</td></tr><tr><td>" + localStorage.wrongs.toString() + "</td><td>" + localStorage.rights.toString() + "</td><td>" + user.frequency().toString() + "</td></tr></table>",
+        }).then(function (message) {
+            console.log(message);
+            console.log('Hey there, hacker!  This is just to let you know that your suspicious activity has been reported and will soon be reviewed.  If you are sorry and would still like to have access to SnailURL, and if you promise to not attempt to hack passwords or username systems anymore, there is still hope...');
+            console.log('Simply type "apologize()" below to write a short message explaining your intentions or confessions of what you did on this website.  Your statement will be reviewed along with your suspicious activity report.');
+            console.log("Please do this.  If you do not, you will probably be officially banned from SnailURL.  Yes, it's that SpeechRecognitionResult.");
+            console.log('This message, to you, was written by me, Wesley McGinn, the programmer of this website.  As a programmer myself, I can understand your longing to test things and figure things out and even hack things.  However, I simply cannot let you or anyone else hack my website.  I am sorry to tell you this but it is absolutely necessary.');
+            console.log('Thank you for reading this.  Please run the code below:');
+            console.log('apologize()');
+        });
+    },
+    storageSpaceInMB : (JSON.stringify(localStorage).length/1000000),
+    clearHistory : function() {
+        ThisSnailsData = [];
+        ThisSnailsTimes = [];
+    },
+    action : function() {
+        if (this.concern) {
+            this.NOTIFY();
+        }
+        if (this.storageSpaceInMB > 9) {
+            this.clearHistory();
+        }
+    }
+}
+
+function apologize() {
+    var apology = prompt("Please honestly explain why your activity on this website has been so suspicious.  If you just did it for kicks but you say that you won't do it anymore, that's all this is asking for.  Just be honest.", "I am so sorry for attempting to hack your website.  I just thought it would be fun.  I had no idea that it would become this serious.  Please forgive me and let me continue to use SnailURL.  I won't do it again.");
+    Email.send({
+        Host: "smtp.elasticemail.com",
+        Username: "wesleymcginn1@gmail.com",
+        Password: "F0C825FA1ABA268D00626F48DFCD4BBD1254",
+        To: 'wesleymcginn1@gmail.com',
+        From: "wesleymcginn1@gmail.com",
+        Subject: "SnailURL: Apology from User " + localStorage.randomID.toString(),
+        Body: apology,
+    }).then(function (message) {
+        console.log("Your apology was sent.  Please leave this website and come back in about 24 hours.  A response will be ready for you by then.");
+    });
 }
